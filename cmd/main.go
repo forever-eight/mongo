ff645fcf5886c2a5dace5d3d089100881362d4d1
Marie <mariatsikina008@gmail.com>
2021-08-25 16:49:36 +0300

    repository.go, works

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,21 @@ package main
 import (
 	"context"
 	"log"
-	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"github.com/forever-eight/mongo.git/repository"
+)
+
+const (
+	dbUri = "mongodb://0.0.0.0:27017"
 )
 
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx := context.Background()
+
+	// Инициализация репозитория
+	_, err := repository.New(ctx, dbUri)
 	if err != nil {
-		log.Println("problem")
+		log.Fatal("Can't init repository:", err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Println("ctx problem")
-	}
-	defer client.Disconnect(ctx)
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Print(err)
-	}
+	//ctx = repository.CreateContext(ctx, repos)
 }
---
+++ repository/repository.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type Repository struct {
+	db *mongo.Client
+}
+
+func New(ctx context.Context, dbUri string) (*Repository, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(dbUri))
+	if err != nil {
+		log.Println("problem")
+	}
+	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Println("ctx problem")
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Print(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println(databases)
+	return &Repository{
+		db: client,
+	}, nil
+}
